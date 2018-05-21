37e76678948f597f50878b6bf41c778dfd042105
changtimwu <changtimwu@gmail.com>
2018-05-21 14:21:00 +0800

    add model to the test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import Invoice from './models/invoice'
 
 it('renders without crashing', () => {
+  const invoice = Invoice.create({ currency: 'TWD' })
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<App invoice={invoice} />, div);
 });
