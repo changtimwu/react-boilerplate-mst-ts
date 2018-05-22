22321a161b653e4fc1a54b67f3795df7802d9c46
changtimwu <changtimwu@gmail.com>
2018-05-22 17:55:47 +0800

    seperate each compoenet into different routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,43 @@
 import * as React from 'react';
 import { observer } from 'mobx-react'
-import './App.css'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { InvoiceType } from './models/invoice'
-import { ItemType } from './models/item'
-import Item from './item'
+
 import EthApp from './EthApp'
 import BootstrapDemo from './BootstrapDemo'
+import InvoiceApp from './InvoiceApp'
 
 interface Props {
   invoice: InvoiceType
 }
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+
 @observer
-class App extends React.Component<Props, ItemType> {
-  private nameInput: HTMLInputElement
-  onSubmit() {
-    this.props.invoice.itemList.add(this.state)
-  }
-
-  onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ name: e.target.value })
-  }
-  onChangeQuantity(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ quantity: parseInt(e.target.value, 10) })
-  }
-  onChangePrice(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ price: parseInt(e.target.value, 10) })
-  }
-
+class App extends React.Component<Props, {}> {
   render() {
     const { invoice } = this.props
     return (
-      <div className="App">
-        <EthApp />
-        <BootstrapDemo />
-        <h1>invoice is {invoice.status()}</h1>
-        <h2>currency = {invoice.currency}</h2>
-        <button onClick={invoice.markPaid}>Pay</button>
-        <form
-          onSubmit={e => {
-            e.preventDefault()
-            this.onSubmit()
-            const elform = e.target as HTMLFormElement
-            elform.reset()
-            this.nameInput.focus()
-          }}
-        >
-          <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              onChange={e => this.onChangeName(e)}
-              ref={(input) => { this.nameInput = input as HTMLInputElement; }}
-            />
-          </label>
-          <label htmlFor="quantity">
-            Quantity
-            <input
-              type="number"
-              id="quantity"
-              onChange={e => this.onChangeQuantity(e)}
-            />
-          </label>
-          <label htmlFor="price">
-            Price
-            <input
-              type="number"
-              id="price"
-              onChange={e => this.onChangePrice(e)}
-            />
-          </label>
-          <button type="submit">Add</button>
-        </form>
-        <ul>
-          {invoice.itemList.items.map((item, i) => (
-            <Item item={item} key={i} />
-          ))}
-        </ul>
-        <h2> {invoice.itemList.len} items and total is ${invoice.itemList.total.toFixed}</h2>
-      </div>
+      <Router>
+        <div>
+          <ul>
+            <li> <Link to="/">Home</Link> </li>
+            <li> <Link to="/ethapp">Eth App</Link></li>
+            <li> <Link to="/invoiceapp">Invoice App</Link></li>
+            <li> <Link to="/bootstrapdemo">Bootstrap Demo</Link></li>
+          </ul>
+          <hr />
+          <Route exact path="/" component={Home} />
+          <Route path="/ethapp" component={EthApp} />
+          <Route path="/invoicedpp" component={InvoiceApp} />
+          <Route path="/bootstrapdemo" component={BootstrapDemo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/InvoiceApp.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { observer } from 'mobx-react'
+import { InvoiceType } from './models/invoice'
+import { ItemType } from './models/item'
+import Item from './item'
+
+interface Props {
+    invoice: InvoiceType
+}
+
+@observer
+class InvoiceApp extends React.Component<Props, ItemType> {
+    private nameInput: HTMLInputElement
+    onSubmit() {
+        this.props.invoice.itemList.add(this.state)
+    }
+
+    onChangeName(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ name: e.target.value })
+    }
+    onChangeQuantity(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ quantity: parseInt(e.target.value, 10) })
+    }
+    onChangePrice(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ price: parseInt(e.target.value, 10) })
+    }
+
+    render() {
+        const { invoice } = this.props
+        return (
+            <div>
+                <h1>invoice is {invoice.status()}</h1>
+                <h2>currency = {invoice.currency}</h2>
+                <button onClick={invoice.markPaid}>Pay</button>
+                <form
+                    onSubmit={e => {
+                        e.preventDefault()
+                        this.onSubmit()
+                        const elform = e.target as HTMLFormElement
+                        elform.reset()
+                        this.nameInput.focus()
+                    }}
+                >
+                    <label htmlFor="name">
+                        Name
+            <input
+                            type="text"
+                            onChange={e => this.onChangeName(e)}
+                            ref={(input) => { this.nameInput = input as HTMLInputElement; }}
+                        />
+                    </label>
+                    <label htmlFor="quantity">
+                        Quantity
+            <input
+                            type="number"
+                            id="quantity"
+                            onChange={e => this.onChangeQuantity(e)}
+                        />
+                    </label>
+                    <label htmlFor="price">
+                        Price
+            <input
+                            type="number"
+                            id="price"
+                            onChange={e => this.onChangePrice(e)}
+                        />
+                    </label>
+                    <button type="submit">Add</button>
+                </form>
+                <ul>
+                    {invoice.itemList.items.map((item, i) => (
+                        <Item item={item} key={i} />
+                    ))}
+                </ul>
+                <h2> {invoice.itemList.len} items and total is ${invoice.itemList.total.toFixed}</h2>
+            </div>
+        );
+    }
+}
+
+export default InvoiceApp
