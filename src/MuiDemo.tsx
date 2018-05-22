ee7419e95fd51b99888367f0fd72c72b199073e8
changtimwu <changtimwu@gmail.com>
2018-05-22 19:04:02 +0800

    replace bootstrap with MaterialUI

---
+++ src/MuiDemo.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import Button from '@material-ui/core/Button'
+
+const MuiDemo = () => (
+    <div>
+        <h2>Material UI Demo</h2>
+        <Button variant="raised" color="primary">
+            Hello World
+      </Button>
+    </div>
+)
+export default MuiDemo
