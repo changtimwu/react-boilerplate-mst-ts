fe3f5fa398f7b5ced5bacd7b59f29fa055fe6935
changtimwu <changtimwu@gmail.com>
2018-05-24 10:50:02 +0800

    adjust component name

--- src/InvoiceEdit.test.tsx
+++ src/InvoiceEdit.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import InvoiceEdit from './InvoiceEdit';
 import Invoice from './models/invoice'
 
 it('renders without crashing', () => {
   const invoice = Invoice.create({ currency: 'TWD' })
   const div = document.createElement('div');
-  ReactDOM.render(<App invoice={invoice} />, div);
+  ReactDOM.render(<InvoiceEdit invoice={invoice} />, div);
 });
