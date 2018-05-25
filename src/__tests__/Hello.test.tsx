fa26ce4767a5d3c36f189c558e3f25b078b1f013
changtimwu <changtimwu@gmail.com>
2018-05-25 15:36:04 +0800

    finish basic enzyme test setup

---
+++ src/__tests__/Hello.test.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { shallow } from "enzyme";
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Hello!</h1>;
+    }
+}
+it("renders the heading", () => {
+    const result = shallow(<Hello />).contains(<h1>Hello!</h1>);
+    expect(result).toBeTruthy();
+});
