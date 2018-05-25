5e77020d00906416f35f05dcc78cbc3697e55444
changtimwu <changtimwu@gmail.com>
2018-05-25 15:56:06 +0800

    rewrite the test in the enzyme form

--- src/__tests__/InvoiceEdit.test.tsx
+++ src/__tests__/InvoiceEdit.test.tsx
@@ -1,10 +1,20 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import InvoiceEdit from './InvoiceEdit';
-import Invoice from './models/invoice'
+import * as React from 'react'
+import { shallow } from "enzyme"
 
-it('renders without crashing', () => {
+import InvoiceEdit from '../InvoiceEdit'
+import Invoice from '../models/invoice'
+
+describe("Invoice", () => {
   const invoice = Invoice.create({ currency: 'TWD' })
-  const div = document.createElement('div');
-  ReactDOM.render(<InvoiceEdit invoice={invoice} />, div);
-});
+  const w = shallow(<InvoiceEdit invoice={invoice} />);
+  describe("Render", () => {
+    it("Pay button is there", () => {
+      let t = w.find('button').first()
+      expect(t.contains('Pay')).toBeTruthy()
+    })
+  })
+  describe('Change Price', () => {
+  })
+  describe('Change Quantity', () => {
+  })
+})
